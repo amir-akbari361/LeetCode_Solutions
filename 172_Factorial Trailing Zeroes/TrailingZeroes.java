30de180a8e27ad0951bb8a326a9c7c8382894e3e
Amir Akbari <amirmahdiakbari@khu.ac.ir>
2025-05-25 23:24:19 +0330

    add solution

---
+++ 172_Factorial Trailing Zeroes/TrailingZeroes.java
@@ -0,0 +1,11 @@
+public class TrailingZeroes {
+    public int trailingZeroes(int n) {
+        int counter=0;
+        while(n>0)
+        {
+            n/=5;
+            counter+=n;
+        }
+        return counter;
+    }
+}
